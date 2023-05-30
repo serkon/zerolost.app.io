64e29e90a25e2479f6660b5e76fb98f0e568f135
Serkan Konakcı <Serkan.Konakci@tubitak.gov.tr>
2023-05-31 00:11:58 +0300

    fix: add BrowserRouter to test file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,11 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Strong/i);
+  render(<BrowserRouter><App /></BrowserRouter>);
+  const linkElement = screen.getByText(/main/i);
 
   expect(linkElement).toBeInTheDocument();
 });
